58da5cb21428a439d1adeb825a4b0535a87acf49
ZOID415 <michaelalfaro93@gmail.com>
2023-02-27 22:29:01 -0800

    adds store.js, apiRoutes.js, htmlRoutes.js, and comments or code

---
+++ db/store.js
@@ -0,0 +1,3 @@
+//This is where you will write functions for API calls if user saved notes as well as well as add notes, remove notes, saved notes.
+
+//
---
+++ routes/htmlRoutes.js
@@ -0,0 +1,2 @@
+// router.get functions (res, req) router grabbing whatever note it is from notes.html 
+//const for path and router.get functions
